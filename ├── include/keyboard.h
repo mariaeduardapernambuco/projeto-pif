b1df0b98e59dd38b7b04b9d7ba38124978a839af
Maria Eduarda Pernambuco Souza Leão <163375829+mariaeduardapernambuco@users.noreply.github.com>
2024-10-31 21:08:39 -0300

    Update keyboard.h

--- ├── include/keyboard.h
+++ ├── include/keyboard.h
@@ -1,19 +1,9 @@
-/**
- * keyboard.h
- * Created on Aug, 23th 2023
- * Author: Tiago Barros
- * Based on "From C to C++ course - 2002"
-*/
-
-#ifndef __KEYBOARD_H__
-#define __KEYBOARD_H__
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
 
 void keyboardInit();
-
 void keyboardDestroy();
-    
 int keyhit();
-
 int readch();
 
-#endif /* __KEYBOARD_H__ */
+#endif
