afbe1e414a7b3da658a1c8790e3a8c8e4a3b4b12
Maria Eduarda Pernambuco Souza Leão <163375829+mariaeduardapernambuco@users.noreply.github.com>
2024-10-31 21:10:56 -0300

    Update timer.h

--- ├── include/timer.h
+++ ├── include/timer.h
@@ -1,16 +1,8 @@
-/**
- * timer.h
- * Created on Aug, 23th 2023
- * Author: Tiago Barros
- * Based on "From C to C++ course - 2002"
-*/
+#ifndef TIMER_H
+#define TIMER_H
 
-void timerInit(int valueMilliSec);
-
+void timerInit(int tick);
+int timerTimeOver();
 void timerDestroy();
 
-void timerUpdateTimer(int valueMilliSec);
-
-int timerTimeOver();
-
-void timerPrint();
+#endif
